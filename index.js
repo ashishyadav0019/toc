022f461e13626bb6f98035861ea060712f15b106
ashish0019 <yadav.ashish@jeevansathi.com>
2020-03-09 17:36:15 +0530

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var server = http.createServer(function (request, response) {
+  var path = '/index.html';
+console.log(path,123);
+  fs.readFile(__dirname + path, function (error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write(error);
+      response.end();
+    } else {
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      response.write(data);
+      response.end();
+    }
+  });
+});
+server.listen(3000);
